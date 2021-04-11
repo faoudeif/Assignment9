9b3d1bf5de7d3ba0cfa8d55dd461ad3374e6c773
faoudeif <77484659+faoudeif@users.noreply.github.com>
2021-04-11 02:07:50 -0400

    Update displayNote.php

--- displayNote.php
+++ displayNote.php
@@ -1,7 +1,10 @@
 <?php
+$notes = "";
+if(count($_POST) > 0){
 require_once 'Date_time.php';
 $dt = new Date_time();
-$notes = $dt->displayNotes();
+$notes = $dt->displayNotes($_POST['begDate'], $_POST['endDate'], $_POST['getnotes']);
+}
 ?>
 
 <!doctype html>
@@ -15,27 +18,34 @@ $notes = $dt->displayNotes();
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   </head>
   <body>
+  <main class="container">
       <div class="container">
         <h1>Display Notes</h1>
+      <form action="displayNote.php" method="post" enctype="multipart/form-data">
 
         <div class="form-group">
-        <p><?php echo $notes ?></p>
+        <a href=addNote.php>Add Note</a>
         </div>
 
         <div class = "form-group">
-        <label for "begDate">Beginning Date</label>
+        <label for = "begDate">Beginning Date</label>
         <input type="date" class="form-control" id="begDate" name="begDate">
         </div>
 
         <div class = "form-group">
-        <label for "endDate">Ending Date</label>
+        <label for = "endDate">Ending Date</label>
         <input type="date" class="form-control" id="endDate" name="endDate">
         </div>
 
         <div class = "form-group">
-        <input type="button" id="getnotes" class="btn btn-primary" value="Get Notes">
+        <input type="submit" name="getnotes" id = "getnotes" class="btn btn-primary" value="Get Notes">
         </div>
 
-    </div>    
+        <div class="form-group">
+        <?php echo $notes;?>
+        </div>
+      </div> 
+    </form>
+  </main>   
   </body>
 </html>
