42e3ecdd42d8a3ff54e5ffa16e8cd00bdac3be1e
faoudeif <77484659+faoudeif@users.noreply.github.com>
2021-04-11 02:08:13 -0400

    Update addNote.php

--- addNote.php
+++ addNote.php
@@ -18,27 +18,30 @@ $notes = $dt->checkSubmit($_POST['dateTime'], $_POST['note']);
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   </head>
   <body>
+  <main class="container">
       <div class="container">
         <h1>Add Note</h1>
-
+      <form action="addNote.php" method="post" enctype="multipart/form-data">
         <div class="form-group">
+        <a href=displayNote.php>Display Notes</a>
         <p><?php echo $notes;?></p>
         </div>
 
         <div class = "form-group">
-        <label for "dateTime">Date and Time</label>
+        <label for = "dateTime">Date and Time</label>
         <input type="datetime-local" class="form-control" id="dateTime" name="dateTime">
         </div>
 
         <div class = "form-group">
-        <label for "note">Note</label>
-        <textarea id = "note" name = "note" cols = "155" rows = "14"></textarea>
+        <label for = "note">Note</label>
+        <textarea id = "note" name = "note" cols = "151" rows = "14"></textarea>
         </div>
 
         <div class = "form-group">
-        <input type="button" id="addnote" class="btn btn-primary" value="Add Note">
+        <input type="submit" name="addnote" id = "addnote" class="btn btn-primary" value="Add Note">
         </div>
-
-    </div>    
+      </div> 
+    </form>
+  </main>   
   </body>
 </html>
